fdf70d1d1276331174b527d9d29d9a95178c9869
Robyn Craig <robynocraig@gmail.com>
2018-07-18 19:42:19 -0500

    got summary & full links to be included in article response

--- public/app.js
+++ public/app.js
@@ -1,11 +1,14 @@
 // Grab the articles as a json
-// $.getJSON("/articles", function(data) {
-//   // For each one
-//   for (var i = 0; i < data.length; i++) {
-//     // Display the apropos information on the page
-//     $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].summary + "<br />" + data[i].link + "</p>");
-//   }
-// });
+$.getJSON("/articles", function(data) {
+  // For each one
+  for (var i = 0; i < data.length; i++) {
+
+    // var domain = ("https://www.audubon.org" + result.link)
+
+    // Display the apropos information on the page
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].summary + "<br />" + "https://www.audubon.org" + data[i].link + "</p>");
+  }
+});
 
 // When the #scrape button is pressed
 $("#scrape").on("click", function() {
